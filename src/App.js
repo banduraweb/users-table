d41dd25deb3de0121492e02850a38cd80ffc6106
bandura <banduraweb@ukr.net>
2020-01-24 09:46:39 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Users List</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

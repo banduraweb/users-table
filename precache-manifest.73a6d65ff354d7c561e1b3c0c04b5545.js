730e45c5056e7d2a1196a7003163216689aeabdc
bandura <banduraweb@ukr.net>
2020-01-26 19:01:26 +0200

    Updates

--- precache-manifest.73a6d65ff354d7c561e1b3c0c04b5545.js
+++ precache-manifest.73a6d65ff354d7c561e1b3c0c04b5545.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c52adb22693ba94f358af3fb037a5419",
+    "revision": "f022fc1df0679a5898b4c31fe4c37165",
     "url": "/users-table/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/users-table/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "6f808b13567039f872e1",
+    "revision": "025a27e9ff1b78a75f1a",
     "url": "/users-table/static/css/main.17ae1c02.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/users-table/static/js/2.bf4838b7.chunk.js.LICENSE"
   },
   {
-    "revision": "6f808b13567039f872e1",
-    "url": "/users-table/static/js/main.db65caf3.chunk.js"
+    "revision": "025a27e9ff1b78a75f1a",
+    "url": "/users-table/static/js/main.cac18f3c.chunk.js"
   },
   {
     "revision": "b7932f2295308d6b8091",
